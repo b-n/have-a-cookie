287fcce3871408facc21424c4b095d29e98821d1
Ben Naylor <ben.naylor@beethree.nl>
2017-11-12 14:11:53 +0100

    a bit more mvc all up in this

---
+++ controllers/weight.js
@@ -0,0 +1,33 @@
+import { getAllUsers, getNewUser, addEntryToUser, setUser, getUserWithoutData } from '../models/weight'
+
+class Weight {
+    constructor() { }
+
+    async addWeight({ event, response }) {
+        const { weight } = event.body
+
+        const existingData = await getAllUsers()
+        const closestUser = existingData.Count ? Weight.getUserClosestToWeight(existingData.Items, weight) : null
+
+        const userToAddWeightTo = closestUser ? closestUser : getNewUser()
+        const updatedRecord = addEntryToUser(userToAddWeightTo, event.body)
+
+        await setUser(updatedRecord)
+
+        return getUserWithoutData(updatedRecord)
+    }
+
+    static getUserClosestToWeight(users, weight) {
+        const sortedUsersByRelevance = users.Items.sort((a, b) => Weight.getRelevanceScore(a, weight) - Weight.getRelevanceScore(b, weight))
+
+        const closestUser = sortedUsersByRelevance[0]
+        return Math.abs(closestUser.weightedAverage - weight) < 5 ? closestUser : null
+    }
+
+    static getRelevanceScore(user, targetWeight) {
+        const { weightedAverage, weightedSd } = user
+        return Math.pow(Math.abs(weightedAverage - targetWeight), 2) / weightedSd
+    }
+}
+
+export default Weight
--- services/weight.js
+++ services/weight.js
@@ -1,76 +1,10 @@
-import AWS from 'aws-sdk'
-import { v4 } from 'uuid'
-import { Executable, Response } from '../lib/sls-sugar'
+import LambdaRouter from 'serverless-lambda-router'
+import Weight from '../controllers/weight'
 
-const dynamoDb = new AWS.DynamoDB.DocumentClient()
-const tableParams = {
-    TableName: process.env.DYNAMODB_TABLE,
-}
+const controller = new Weight()
 
-class Weight extends Executable {
-    constructor() {
-        super()
-    }
+const router = new LambdaRouter()
 
-    async httpPost(data) {
-        const { weight } = data
+router.get('/weights', controller.addWeight)
 
-        const existingData = await this.getAllData()
-
-        const closestRecord = existingData.Count
-            ? existingData.Items.sort((a, b) => this.getRelevanceScore(a, weight) - this.getRelevanceScore(b, weight))[0]
-            : null
-
-        const isUpdate = closestRecord && Math.abs(closestRecord.weightedAverage - weight) < 5
-
-        const newRecord = this.addWeight(isUpdate ? closestRecord : this.getNewUser(), data)
-
-        await dynamoDb.put({ ...tableParams, Item: newRecord }).promise()
-
-        return newRecord
-    }
-
-    addWeight(user, data) {
-        const { weight } = data
-        return {
-            ...user,
-            weightedAverage: weight,
-            weightedSd: 0.01,
-            data: [
-                ...user.data,
-                data,
-            ],
-        }
-    }
-
-    getNewUser() {
-        const id = v4()
-        return {
-            id,
-            name: id,
-            data: [],
-        }
-    }
-
-    getRelevanceScore(user, targetWeight) {
-        const { weightedAverage, weightedSd } = user
-        return Math.pow(Math.abs(weightedAverage - targetWeight), 2) / weightedSd
-    }
-
-    getAllData() {
-        return dynamoDb.scan(tableParams).promise()
-    }
-
-    handle(props) {
-        const { httpMethod, body } = props.event
-
-        switch (httpMethod) {
-            case 'POST':
-                return this.httpPost(body)
-            default:
-                return new Response('Method ' + httpMethod + ' does not exist', 405)
-        }
-    }
-}
-
-export const handle = (event, context, callback) => new Weight().run({ event, context, callback })
+export const handler = router.handler()
