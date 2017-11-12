d2cbe51f7cf0647d586efbfc024508b2127071d8
Ben Naylor <ben.naylor@beethree.nl>
2017-11-12 09:39:21 +0100

    not used, so remove these

--- webpack.config.js
+++ webpack.config.js
@@ -4,7 +4,6 @@ const slsw = require('serverless-webpack');
 module.exports = {
     entry: slsw.lib.entries,
     target: 'node',
-    devtool: 'source-map',
     externals: [nodeExternals()],
     module: {
         rules: [
